27ddfc6aa628d2af43b306f4b93861da2fea3dda
Shervin Aflatooni <shervinaflatooni@gmail.com>
2016-05-01 21:31:23 +1000

    Add new string

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main(int argc, char **argv) {
   std::cout << "Hello world" << std::endl;
   std::cout << "How are you" << std::endl;
   std::cout << "How are poo" << std::endl;
+  std::cout << "How are today?" << std::endl;
   return 0;
 }
