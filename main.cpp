0973b76c578ba4b37b13815eede05a25a154d45e
Shervin Aflatooni <shervinaflatooni@gmail.com>
2016-05-01 21:28:30 +1000

    Initial Game Commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+  std::cout << "Hello world" << std::endl;
+  std::cout << "How are you" << std::endl;
+  std::cout << "How are poo" << std::endl;
+  return 0;
+}
